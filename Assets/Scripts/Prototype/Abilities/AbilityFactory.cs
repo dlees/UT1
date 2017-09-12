f494d642028371c375b20b339805f71a175b084f
dlees <dlees@umich.edu>
2017-09-11 23:07:39 -0700

    Fire Ability

--- Assets/Scripts/Prototype/Abilities/AbilityFactory.cs
+++ Assets/Scripts/Prototype/Abilities/AbilityFactory.cs
@@ -9,9 +9,10 @@ public class AbilityFactory : MonoBehaviour {
 	public Ability createAbility (string name, Combatant performer, List<Combatant> targets) {
 	
 		switch (name) {
-		case "Attack":
-			return new AttackAbility (performer, targets[0]);
-
+		    case "Attack":
+			    return new AttackAbility (performer, targets[0]);
+            case "Fire":
+                return new FireAbility(performer, targets[0], arena);
 		}
 	
 		throw new UnityException (name + " is not an ability.");
